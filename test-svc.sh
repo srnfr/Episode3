c66453592ec2a6f52244495fb635b1fedceafd21
srnfr <stephane@reytan.fr>
2022-04-18 17:54:42 +0200

    fix

--- test-svc.sh
+++ test-svc.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 DEBUGPODNAME=debug$RANDOM
-kubectl run $DEBUGPODNAME --image=wbitt/network-multitool --restart=Never
+kubectl run $DEBUGPODNAME --image=wbitt/network-multitool --restart=Never -l type=debug
 
 echo "Waiting for creation to complete..."
 kubectl wait --for=condition=Ready pod/$DEBUGPODNAME
 
-for ns in blue red green; do
+for ns in red green blue; do
 	svc=svc-echoserver.${ns}.svc
 	echo "-----------"
 	echo "Test vers ${svc}..."
-	kubectl exec -it $DEBUGPODNAME -- curl -sv -o /dev/null http://${svc}
+	kubectl exec -it $DEBUGPODNAME -- curl -sv --connect-timeout 3 -o /dev/null http://${svc}
 	echo ""
 	echo ""
 done
