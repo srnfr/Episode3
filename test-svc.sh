93f76d26811a544017e48500329d9fbe6b3d28cf
srnfr <stephane@reytan.fr>
2022-04-18 18:08:38 +0200

    label change

--- test-svc.sh
+++ test-svc.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
+NSDEBUG="default"
+
 DEBUGPODNAME=debug$RANDOM
-kubectl run $DEBUGPODNAME --image=wbitt/network-multitool --restart=Never -l type=debug
+kubectl run $DEBUGPODNAME --image=wbitt/network-multitool --restart=Never -l type=debug -n $NSDEBUG
 
 echo "Waiting for creation to complete..."
-kubectl wait --for=condition=Ready pod/$DEBUGPODNAME
+kubectl wait --for=condition=Ready pod/$DEBUGPODNAME -n $NSDEBUG
+
+kubectl get pod/$DEBUGPODNAME --show-labels -n $NSDEBUG
 
 for ns in red green blue; do
 	svc=svc-echoserver.${ns}.svc
 	echo "-----------"
 	echo "Test vers ${svc}..."
-	kubectl exec -it $DEBUGPODNAME -- curl -sv --styled-output --connect-timeout 2 -o /dev/null http://${svc}
+	kubectl exec -it $DEBUGPODNAME -n $NSDEBUG -- curl -sv --styled-output --connect-timeout 2 -o /dev/null http://${svc}
 	echo ""
 	echo ""
 done
