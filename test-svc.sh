23f30afad36c39c3e2503925075422ac75458206
srnfr <stephane@reytan.fr>
2022-04-19 08:26:14 +0200

    last

--- test-svc.sh
+++ test-svc.sh
@@ -7,6 +7,7 @@ DEBUGPODNAME=debug$RANDOM
 kubectl run $DEBUGPODNAME --image=wbitt/network-multitool --restart=Never -l type=$LABEL -n $NSDEBUG
 
 echo "Waiting for creation to complete..."
+echo "Namespace = $NSDEBUG"
 kubectl wait --for=condition=Ready pod/$DEBUGPODNAME -n $NSDEBUG
 
 kubectl get pod/$DEBUGPODNAME --show-labels -n $NSDEBUG
@@ -15,7 +16,7 @@ for ns in red green blue; do
 	svc=svc-echoserver.${ns}.svc
 	echo "-----------"
 	echo "Test vers ${svc}..."
-	kubectl exec -it $DEBUGPODNAME -n $NSDEBUG -- curl -sv --styled-output --connect-timeout 2 -o /dev/null http://${svc}
+	kubectl exec -it $DEBUGPODNAME -n $NSDEBUG -- curl -s -i -w "%{http_code}" --connect-timeout 2 -o /dev/null http://${svc}
 	echo ""
 	echo ""
 done
