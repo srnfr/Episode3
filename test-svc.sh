b60a51648c59d7840ec00a0265b770f4413c9072
srnfr <stephane@reytan.fr>
2022-04-19 21:52:07 +0200

    add ns select

--- test-svc.sh
+++ test-svc.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
 
-NSDEBUG="blue"
-LABEL="null"
 
-DEBUGPODNAME=debug$RANDOM
-kubectl run $DEBUGPODNAME --image=wbitt/network-multitool --restart=Never -l type=$LABEL -n $NSDEBUG
+for nsd in default blue; do
 
-echo "Waiting for creation to complete..."
-echo "Namespace = $NSDEBUG"
-kubectl wait --for=condition=Ready pod/$DEBUGPODNAME -n $NSDEBUG
+	NSDEBUG=$nsd
+	LABEL="null"
 
-kubectl get pod/$DEBUGPODNAME --show-labels -n $NSDEBUG
+	DEBUGPODNAME=debug$RANDOM
 
-for ns in red green blue; do
-	svc=svc-echoserver.${ns}.svc
-	echo "-----------"
-	echo "Test vers ${svc}..."
-	kubectl exec -it $DEBUGPODNAME -n $NSDEBUG -- curl -s -i -w "%{http_code}" --connect-timeout 2 -o /dev/null http://${svc}
-	echo ""
-	echo ""
+	echo "#########################@"
+	kubectl run $DEBUGPODNAME --image=wbitt/network-multitool --restart=Never -l type=$LABEL -n $NSDEBUG
+
+	echo "Waiting for creation to complete..."
+	echo "Namespace = $NSDEBUG"
+	kubectl wait --for=condition=Ready pod/$DEBUGPODNAME -n $NSDEBUG
+
+	kubectl get pod/$DEBUGPODNAME --show-labels -n $NSDEBUG
+
+	for ns in red green blue; do
+		svc=svc-echoserver.${ns}.svc
+		echo "-----------"
+		echo "Test vers ${svc}..."
+		kubectl exec -it $DEBUGPODNAME -n $NSDEBUG -- curl -s -i -w "%{http_code}" --connect-timeout 2 -o /dev/null http://${svc}
+		echo ""
+		echo ""
+	done
+
+	kubectl delete pod/$DEBUGPODNAME --wait=false
+
 done
-
-kubectl delete pod/$DEBUGPODNAME --wait=false
