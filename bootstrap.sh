c337e189caf9b1cedaf76b73891d8115b92adbe5
srnfr <stephane@reytan.fr>
2022-04-20 20:45:41 +0200

    denyall

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,9 +13,10 @@ helm repo add argo https://argoproj.github.io/argo-helm
 
 helm install myargo argo/argo-cd -f helm-argocd-values.yaml --create-namespace --namespace=argocd
 
-kubectl wait --for=condition=Ready pod/myargo-argocd-server -n argocd
+
+
+kubectl wait --for=condition=Ready pods -n argocd -l app.kubernetes.io/name=argocd-server 
 
 kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
 
-kubectl wait --for=condition=Ready pod/$DEBUGPODNAME -n $NSDEBUG
-kubectl port-forward service/myargo-argocd-server -n argocd 8080:443
+kubectl port-forward service/myargo-argocd-server -n argocd 8080:443 &
