03b51474615327cd857b98c5d987807ea8c09567
srnfr <stephane@reytan.fr>
2022-04-21 22:10:21 +0200

    change NS

--- test-svc.sh
+++ test-svc.sh
@@ -12,7 +12,7 @@ for nsd in default blue; do
 	kubectl run $DEBUGPODNAME --image=wbitt/network-multitool --restart=Never -l type=$LABEL -n $NSDEBUG
 
 	echo "Waiting for creation to complete..."
-	echo "Namespace = $NSDEBUG"
+	##echo "Namespace = $NSDEBUG"
 	kubectl wait --for=condition=Ready pod/$DEBUGPODNAME -n $NSDEBUG
 
 	kubectl get pod/$DEBUGPODNAME --show-labels -n $NSDEBUG
@@ -20,7 +20,7 @@ for nsd in default blue; do
 	for ns in red green blue; do
 		svc=svc-echoserver.${ns}.svc
 		echo "-----------"
-		echo "Test vers ${svc}..."
+		echo -n "Requete : NS=$NSDEBUG => ${svc}...  "
 		kubectl exec -it $DEBUGPODNAME -n $NSDEBUG -- curl -s -i -w "%{http_code}" --connect-timeout 2 -o /dev/null http://${svc}
 		echo ""
 	done
