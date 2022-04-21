9f0c15315a9ddf9b7c337a8ec295792116f77457
srnfr <stephane@reytan.fr>
2022-04-21 22:34:27 +0200

    fix

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,6 @@
 for ns in blue green red; do
 	echo "Deploiement dans NS $ns.."
 	kubectl create ns ${ns}
-	kubectl apply -f webdemo.yml -n ${ns}
 done
 
 ## ARGO-CD
@@ -13,10 +12,8 @@ helm repo add argo https://argoproj.github.io/argo-helm
 
 helm install myargo argo/argo-cd -f helm-argocd-values.yaml --create-namespace --namespace=argocd
 
-
-
 kubectl wait --for=condition=Ready pods -n argocd -l app.kubernetes.io/name=argocd-server 
 
 kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
-
+echo ""
 kubectl port-forward service/myargo-argocd-server -n argocd 8080:443 &
